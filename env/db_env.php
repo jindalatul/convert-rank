d4dae7e3119f46db42df1af85a84e3b177e1d947
Atul Jindal <er.atul.jindal@gmail.com>
2025-10-17 15:56:28 -0700

    Create db_env.php

---
+++ env/db_env.php
@@ -0,0 +1,8 @@
+<?php
+// app/db_env.php — DO NOT COMMIT TO GIT
+return [
+    'DB_HOST'     => 'lamp-docker-db-1',
+    'DB_NAME'     => 'hub-spoke',
+    'DB_USER'     => 'root',
+    'DB_PASSWORD' => 'root_password',
+];
