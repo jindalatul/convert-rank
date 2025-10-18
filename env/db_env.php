ecacf168476aa0c77447193aae6d2eb3deba1a95
Atul Jindal <er.atul.jindal@gmail.com>
2025-10-17 17:02:47 -0700

    Working code

--- env/db_env.php
+++ env/db_env.php
@@ -2,7 +2,7 @@
 // app/db_env.php — DO NOT COMMIT TO GIT
 return [
     'DB_HOST'     => 'lamp-docker-db-1',
-    'DB_NAME'     => 'hub-spoke',
-    'DB_USER'     => 'root',
-    'DB_PASSWORD' => 'root_password',
+    'DB_NAME'     => '',
+    'DB_USER'     => '',
+    'DB_PASSWORD' => '',
 ];
--- env/google-login-env.php
+++ env/google-login-env.php
@@ -5,8 +5,8 @@ Generate credentials here
 https://console.cloud.google.com/apis/credentials
 */
 return [
-    'GOOGLE_CLIENT_ID'     => '390929839517-0dqaic9a51nqr2c22k1p7m82appckd6d.apps.googleusercontent.com',
-    'GOOGLE_CLIENT_SECRET' => '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',
+    'GOOGLE_CLIENT_ID'     => '',
+    'GOOGLE_CLIENT_SECRET' => '',
     'GOOGLE_REDIRECT_URI'  => 'http://localhost/convert-rank/app/google-callback.php',
     'HOST_NAME'            => 'http://localhost/convert-rank/app'
 ];
